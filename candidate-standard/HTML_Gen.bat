3883c370dd3288dfbb5cd9cc33db4a60d79b9ff7
Gobe Hobona <ghobona@opengeospatial.org>
2021-09-16 16:28:58 +0100

    Uploads published v1.0.0 standard and Removes candidate standard folder

--- candidate-standard/HTML_Gen.bat
+++
@@ -1,1 +0,0 @@
-asciidoctor --trace -o ../19-086.html EDR-candidate-specification.adoc
--- candidate-standard/PDF_Gen.bat
+++
@@ -1,1 +0,0 @@
-asciidoctor -r asciidoctor-pdf --trace -b pdf -o ../19-086.pdf EDR-candidate-specification.adoc
